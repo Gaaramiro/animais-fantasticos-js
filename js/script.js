d21618ba1dbe76f6ad0dc39356849ddd102dd88e
Gaaramiro <gaaramiro@live.com>
2024-10-11 07:39:01 +0200

    corrigido caminho api animaisapi.json

--- js/script.js
+++ js/script.js
@@ -40,5 +40,5 @@ menuMobile.init();
 
 initFuncionamento();
 
-fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchAnimais("./animaisapi.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
